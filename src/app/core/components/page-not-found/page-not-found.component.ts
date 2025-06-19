d29339dbf267be548e45370bd204981a31299c5d
matthewsrt29 <matthew.tr29@gmail.com>
2025-06-18 23:43:23 -0500

    feat:page not found

--- src/app/core/components/page-not-found/page-not-found.component.ts
+++ src/app/core/components/page-not-found/page-not-found.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule], // ← Esto es clave
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.css'
+  styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent {
 
+  constructor(private router: Router) {}
+
+  getHomeRoute(): string {
+    const currentUrl = this.router.url;
+
+    if (currentUrl.includes('/businessman')) {
+      return '/businessman/inicio';
+    } else if (currentUrl.includes('/supplier')) {
+      return '/supplier/inicio';
+    } else {
+      return '/inicio';
+    }
+  }
 }
